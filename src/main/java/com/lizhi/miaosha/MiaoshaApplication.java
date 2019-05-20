972e3b60cfc4ad9f80c11be43cfe5b726a53afa2
lenovo <15001295170@163.com>
2019-05-20 12:08:25 +0800

    修改主类名

--- src/main/java/com/lizhi/miaosha/MiaoshaApplication.java
+++ src/main/java/com/lizhi/miaosha/MiaoshaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootMiaoshaApplication {
+public class MiaoshaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMiaoshaApplication.class, args);
+		SpringApplication.run(MiaoshaApplication.class, args);
 	}
 
 }
--- src/test/java/com/lizhi/miaosha/MiaoshaApplicationTests.java
+++ src/test/java/com/lizhi/miaosha/MiaoshaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootMiaoshaApplicationTests {
+public class MiaoshaApplicationTests {
 
 	@Test
 	public void contextLoads() {
